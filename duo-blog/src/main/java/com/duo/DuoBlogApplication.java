baa3dc04e06e6ae58ede0a37bd65160ed368cf79
HCDUO <2650591287@qq.com>
2023-07-26 14:28:55 +0800

    Initial commit

---
+++ duo-blog/src/main/java/com/duo/DuoBlogApplication.java
@@ -0,0 +1,20 @@
+package com.duo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <pre>
+ *
+ * </pre>
+ *
+ * @author <a href="https://github.com/HCDUO">HCDUO</a>
+ * @date:2023/7/26 1:49
+ */
+
+@SpringBootApplication
+public class DuoBlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DuoBlogApplication.class,args);
+    }
+}
