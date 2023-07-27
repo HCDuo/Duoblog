71c21382aeb77a56d8ed35a049b6de53a982c709
HCDUO <2650591287@qq.com>
2023-07-27 17:10:59 +0800

    login 登录

---
+++ duo-blog/src/main/java/com/duo/controller/BlogLoginController.java
@@ -0,0 +1,28 @@
+package com.duo.controller;
+
+import com.duo.domain.ResponseResult;
+import com.duo.domain.entity.User;
+import com.duo.service.BlogLoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <pre>
+ *
+ * </pre>
+ *
+ * @author <a href="https://github.com/HCDUO">HCDUO</a>
+ * @date:2023/7/28 0:15
+ */
+@RestController
+public class BlogLoginController {
+    @Autowired
+    private BlogLoginService blogLoginService;
+
+    @PostMapping("/login")
+    public ResponseResult login(@RequestBody User user){
+        return blogLoginService.login(user);
+    }
+}
---
+++ duo-framework/src/main/java/com/duo/service/impl/BlogLoginServiceImpl.java
@@ -0,0 +1,58 @@
+package com.duo.service.impl;
+
+import com.duo.domain.ResponseResult;
+import com.duo.domain.entity.LoginUser;
+import com.duo.domain.entity.User;
+import com.duo.domain.vo.BlogUserLoginVo;
+import com.duo.domain.vo.UserInfoVo;
+import com.duo.service.BlogLoginService;
+import com.duo.utils.BeanCopyUtils;
+import com.duo.utils.JwtUtil;
+import com.duo.utils.RedisCache;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+/**
+ * <pre>
+ *
+ * </pre>
+ *
+ * @author <a href="https://github.com/HCDUO">HCDUO</a>
+ * @date:2023/7/28 0:17
+ */
+@Service
+public class BlogLoginServiceImpl implements BlogLoginService {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private RedisCache redisCache;
+
+    @Override
+    public ResponseResult login(User user) {
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(user.getUserName(),user.getPassword());
+        Authentication authenticate = authenticationManager.authenticate(authenticationToken);
+        //判断是否认证通过
+        if(Objects.isNull(authenticate)){
+            throw new RuntimeException("用户名或密码错误");
+        }
+        //获取userid 生成token
+        LoginUser loginUser = (LoginUser) authenticate.getPrincipal();
+        String userId = loginUser.getUser().getId().toString();
+        String jwt = JwtUtil.createJWT(userId);
+        //把用户信息存入redis
+        redisCache.setCacheObject("bloglogin:"+userId,loginUser);
+
+        //把token和userinfo封装 返回
+        //把User转换成UserInfoVo
+        UserInfoVo userInfoVo = BeanCopyUtils.copyBean(loginUser.getUser(), UserInfoVo.class);
+        BlogUserLoginVo vo = new BlogUserLoginVo(jwt,userInfoVo);
+        return ResponseResult.okResult(vo);
+    }
+}
