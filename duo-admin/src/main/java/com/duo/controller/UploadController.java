525c4d36cfac0c931e390431dd059895cec41ea9
HCDUO <2650591287@qq.com>
2023-08-02 17:29:49 +0800

    upload 上传图片接口

---
+++ duo-admin/src/main/java/com/duo/controller/UploadController.java
@@ -0,0 +1,31 @@
+package com.duo.controller;
+
+import com.duo.domain.ResponseResult;
+import com.duo.service.UploadService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+/**
+ * <pre>
+ *
+ * </pre>
+ *
+ * @author <a href="https://github.com/HCDUO">HCDUO</a>
+ * @date:2023/8/2 17:27
+ */
+@RestController
+public class UploadController {
+
+    @Autowired
+    private UploadService uploadService;
+
+    @PostMapping("/upload")
+    public ResponseResult uploadImg(@RequestParam("img") MultipartFile multipartFile) throws IOException {
+        return uploadService.uploadImg(multipartFile);
+    }
+}
